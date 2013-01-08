ae414143a14a9b902f5a08273f252d19f2af0eb8
darkmasta <alperen_bagci@hotmail.com>
2013-01-08 16:13:21 +0200

    initial commit of basic twitter/bootstrap site

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'sqlite3'
+
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end 
+
+gem 'jquery-rails', '2.0.2'
+gem 'execjs'
+gem 'therubyracer' 
+gem 'less-rails-bootstrap', '~> 2.0'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuhBlog::Application.config.secret_token = '87931f3c762eb403a9059413fddfa1c4756a63a5861b4b94b2e7e76afce0143c8d969744d790d002b76ca65c50083c9693e86f13a58e5150db16069ccd27e727'
